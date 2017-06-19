b349f8cc2284a6480bfcd2ec31051fc0ba5c6170
Taras <murzenkovtn@gmail.com>
2017-06-19 13:03:16 +0300

    tests

---
+++ src/app/1-voter/voter.component.spec.ts
@@ -0,0 +1,14 @@
+import { VoterComponent } from './voter.component';
+
+describe('VoterComponent', () => {
+
+  beforeEach(() => {
+    let component = new VoterComponent();
+    component.othersVote = 20;
+    component.m
+  });
+
+  it('should render total votes', () => {
+    component
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {VoterComponent} from './1-voter/voter.component';
+import {UsersComponent} from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, VoterComponent, UsersComponent
   ],
   imports: [
     BrowserModule,
